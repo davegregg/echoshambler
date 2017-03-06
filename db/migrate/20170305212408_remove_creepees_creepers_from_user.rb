b6b03a7d16dac1ae26d97c3ec0b409c1c90509ba
davegregg <davegregg@gmail.com>
2017-03-05 22:18:25 -0500

    Removed old contradictory migrations

--- db/migrate/20170305212408_remove_creepees_creepers_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCreepeesCreepersFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :creepees, :string
-    remove_column :users, :creepers, :string
-  end
-end
--- db/migrate/20170305212844_remove_creepees_creepers_to_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCreepeesCreepersToUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :creepees_count, :string
-    remove_column :users, :creepers_count, :string
-  end
-end
