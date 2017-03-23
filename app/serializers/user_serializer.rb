27f8fde5f4be9b9fc2f3d9d13e615fc3f8c300d0
davegregg <davegregg@gmail.com>
2017-03-23 06:49:35 -0400

    Added grunts_count to users serializer for realzies

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :fullname, :photo_url, :creepers_count, :creepees_count, :created_at
+  attributes :id, :username, :fullname, :photo_url, :creepers_count, :creepees_count, :created_at, :grunts_count
   has_many :grunts
 end
